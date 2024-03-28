75e34c047191ad81cb5aab9eca2c61d5088d0d26
wanoo21 <ionprodan33@gmail.com>
2024-03-28 11:16:47 +0200

    feat: more info on first page

--- src/consts.ts
+++ src/consts.ts
@@ -2,21 +2,17 @@ export const SITE_DATA = {
     title: 'Free Angular Email Template Builder (former wlocalhost)',
     description: 'Powerful Angular Library for Creating Customizable Email Builders, now open source.',
     shortDescription: 'Powerful Angular Library for Creating Customizable Email Builders',
-    slogan: 'Made by Developers, for Developers',
 
 
     twitterUrl: 'https://twitter.com/NgbEmail',
     discordUrl: 'https://discord.gg/S6Yx6yNZSG',
     // affiliateUrl: 'https://wlocalhost.gumroad.com/affiliates',
     githubUrl: 'https://github.com/wanoo21/ngb.email',
-    // licenseUrl: 'https://wlocalhost.gumroad.com/l/ngb-14',
+    converterUrl: 'https://github.com/wanoo21/Angular-mjml-output',
     npmUrl: 'https://www.npmjs.com/package/@wlocalhost/ngx-email-builder',
-    email: 'support@ngb.email',
+    email: 'me@yon.fun',
 
-    shortName: 'NGB Email Builder',
-    showStickyCTA: false,
     ogImage: '/ngb-email-builder.png',
-    price: 0,
 }
 
 // Themes
@@ -62,83 +58,3 @@ export const THEMES = [
         demoUrl: 'https://prime.ngb.email'
     }
 ];
-
-// FAQ
-export const FAQ = [
-    {
-        "question": "Can I use the NGB Email Builder in multiple projects or domains under a single license?",
-        "answer": "The [NGB Email Builder](/) license is valid for one domain only. If you require usage in multiple projects or domains, separate licenses need to be obtained for each."
-    },
-    {
-        "question": "Are there any limitations or restrictions on commercial usage of the NGB Email Builder?",
-        "answer": "There are no specific limitations or restrictions on commercial usage of the NGB Email Builder. You can use it commercially without any issues."
-    },
-    {
-        "question": "Is the source code of the NGB Email Builder accessible to customers?",
-        "answer": "The source code of the NGB Email Builder library is not publicly accessible. It is proprietary and not open source."
-    },
-    {
-        "question": "Can I customize and modify the source code of the library to suit my specific needs?",
-        "answer": "Modifying the source code of the NGB Email Builder is not supported. However, you can customize the [email builder templates](#themes) and utilize the provided customization options within the library."
-    },
-    {
-        "question": "What are the terms and conditions associated with the NGB Email Builder license?",
-        "answer": "The terms and conditions associated with the NGB Email Builder license are in accordance with the [license agreement](https://wlocalhost.gumroad.com/p/ngb-email-builder-license-agreement)."
-    },
-    {
-        "question": "Does the NGB Email Builder offer integration with third-party email service providers?",
-        "answer": "At the moment, the NGB Email Builder does not offer direct integrations with third-party providers. However, you can export the email templates created with the builder and import them into your preferred email service provider."
-    },
-    {
-        "question": "Are there any additional costs or fees beyond the initial purchase of the library?",
-        "answer": "There are no additional costs or fees beyond the initial purchase of the NGB Email Builder library. The license fee covers the usage of the library and ongoing support."
-    },
-    {
-        "question": "What type of support is provided for customers using the NGB Email Builder?",
-        "answer": "We provide support for the NGB Email Builder through our Discord community or via email. You can join our [Discord server](https://discord.gg/S6Yx6yNZSG) for community support and direct interaction with our team. Alternatively, you can reach out to us via [email](mailto:support@wlocalhost.org) for personalized assistance."
-    },
-    {
-        "question": "Can I receive regular updates and new features for the NGB Email Builder after the initial purchase?",
-        "answer": "Yes, we continuously work on improving and adding new features to the NGB Email Builder. You will receive regular updates and access to new features as part of the ongoing support for the library."
-    }
-];
-
-// Features
-export const FEATURES = [
-    {
-        "name": "Drag & Drop Email Builder",
-        "description": "Experience the ease of email creation with our user-friendly drag-and-drop interface. No need to rely on your developers; you can effortlessly design professional-looking emails by simply dragging pre-built email components, rearranging content blocks, and customizing layouts.",
-        "icon": "<svg class=\"w-5 h-5 flex-none text-red-600\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 20 20\" fill=\"currentColor\">\n<path d=\"M14 17h2.75A2.25 2.25 0 0019 14.75v-9.5A2.25 2.25 0 0016.75 3H14v14zM12.5 3h-5v14h5V3zM3.25 3H6v14H3.25A2.25 2.25 0 011 14.75v-9.5A2.25 2.25 0 013.25 3z\" />\n</svg>",
-        // "url": "/"
-    },
-    {
-        "name": "Customizable Email Builder Templates",
-        "description": "We handle everything from creating your email builder templates to customizing them according to your brand's style and requirements. Whether you prefer a pre-built template that you can customize or want a template designed from scratch, our team will ensure your templates match your brand's aesthetics.",
-        "icon": "<svg class=\"w-5 h-5 flex-none text-red-600\" fill=\"currentColor\" viewBox=\"0 0 20 20\" xmlns=\"http://www.w3.org/2000/svg\">\n<path clip-rule=\"evenodd\" d=\"M3.5 2A1.5 1.5 0 002 3.5V15a3 3 0 106 0V3.5A1.5 1.5 0 006.5 2h-3zm11.753 6.99L9.5 14.743V6.257l1.51-1.51a1.5 1.5 0 012.122 0l2.121 2.121a1.5 1.5 0 010 2.122zM8.364 18H16.5a1.5 1.5 0 001.5-1.5v-3a1.5 1.5 0 00-1.5-1.5h-2.136l-6 6zM5 16a1 1 0 100-2 1 1 0 000 2z\" fill-rule=\"evenodd\" />\n</svg>",
-        // "url": "/templates/custom-templates"
-    },
-    {
-        "name": "Custom Email Blocks",
-        "description": "Our team specializes in creating unique and specialized email content by designing custom email blocks. We'll work closely with you to understand your specific needs and requirements, whether it's adding product showcases, social media feeds, or personalized recommendations, and create custom email blocks that truly resonate with your audience.",
-        "icon": "<svg class=\"w-5 h-5 flex-none text-red-600\" fill=\"currentColor\" viewBox=\"0 0 20 20\" xmlns=\"http://www.w3.org/2000/svg\">\n<path d=\"M2 4.25A2.25 2.25 0 014.25 2h2.5A2.25 2.25 0 019 4.25v2.5A2.25 2.25 0 016.75 9h-2.5A2.25 2.25 0 012 6.75v-2.5zM2 13.25A2.25 2.25 0 014.25 11h2.5A2.25 2.25 0 019 13.25v2.5A2.25 2.25 0 016.75 18h-2.5A2.25 2.25 0 012 15.75v-2.5zM11 4.25A2.25 2.25 0 0113.25 2h2.5A2.25 2.25 0 0118 4.25v2.5A2.25 2.25 0 0115.75 9h-2.5A2.25 2.25 0 0111 6.75v-2.5zM15.25 11.75a.75.75 0 00-1.5 0v2h-2a.75.75 0 000 1.5h2v2a.75.75 0 001.5 0v-2h2a.75.75 0 000-1.5h-2v-2z\" />\n</svg>",
-        // "url": "/blocks/custom-block"
-    },
-    {
-        "name": "Role-Based Access Control",
-        "description": "Leave the management of user access and permissions to us with our role-based access control feature. We'll define user roles and assign appropriate permissions, ensuring that only authorized individuals can access and modify your email templates. Rest easy knowing that your templates are secure and accessible to the right people.",
-        "icon": "<svg class=\"w-5 h-5 flex-none text-red-600\" fill=\"currentColor\" viewBox=\"0 0 20 20\" xmlns=\"http://www.w3.org/2000/svg\">\n<path d=\"M7 8a3 3 0 100-6 3 3 0 000 6zM14.5 9a2.5 2.5 0 100-5 2.5 2.5 0 000 5zM1.615 16.428a1.224 1.224 0 01-.569-1.175 6.002 6.002 0 0111.908 0c.058.467-.172.92-.57 1.174A9.953 9.953 0 017 18a9.953 9.953 0 01-5.385-1.572zM14.5 16h-.106c.07-.297.088-.611.048-.933a7.47 7.47 0 00-1.588-3.755 4.502 4.502 0 015.874 2.636.818.818 0 01-.36.98A7.465 7.465 0 0114.5 16z\" />\n</svg>",
-        // "url": "/services/aipemailbuildermiddlewareservice"
-    },
-    {
-        "name": "Angular i18n Support",
-        "description": "Our email builder provides seamless localization and internationalization capabilities with built-in Angular i18n support. With our expertise, we'll ensure your email builder is easily translated into multiple languages, allowing you to effectively engage a global audience.",
-        "icon": "<svg class=\"w-5 h-5 flex-none text-red-600\" fill=\"currentColor\" viewBox=\"0 0 20 20\" xmlns=\"http://www.w3.org/2000/svg\">\n<path d=\"M7.75 2.75a.75.75 0 00-1.5 0v1.258a32.987 32.987 0 00-3.599.278.75.75 0 10.198 1.487A31.545 31.545 0 018.7 5.545 19.381 19.381 0 017 9.56a19.418 19.418 0 01-1.002-2.05.75.75 0 00-1.384.577 20.935 20.935 0 001.492 2.91 19.613 19.613 0 01-3.828 4.154.75.75 0 10.945 1.164A21.116 21.116 0 007 12.331c.095.132.192.262.29.391a.75.75 0 001.194-.91c-.204-.266-.4-.538-.59-.815a20.888 20.888 0 002.333-5.332c.31.031.618.068.924.108a.75.75 0 00.198-1.487a32.832 32.832 0 00-3.599-.278V2.75z\" />\n</svg>",
-        // "url": "/getting-started/angular-i18n-support"
-    },
-    {
-        "name": "MJML Integration",
-        "description": "Integrate MJML, a powerful markup language for responsive email templates, seamlessly into your email builder. Our team will ensure that your email builder seamlessly works with MJML, empowering you to create stunning, mobile-responsive email templates with ease.",
-        "icon": "<svg class=\"w-5 h-5 flex-none text-red-600\" fill=\"currentColor\" viewBox=\"0 0 20 20\" xmlns=\"http://www.w3.org/2000/svg\">\n<path clip-rule=\"evenodd\" d=\"M2.5 3A1.5 1.5 0 001 4.5V15a3 3 0 106 0V3.5A1.5 1.5 0 006.5 2h-3zm11.753 6.99L9.5 14.743V6.257l1.51-1.51a1.5 1.5 0 012.122 0l2.121 2.121a1.5 1.5 0 010 2.122zM8.364 18H16.5a1.5 1.5 0 001.5-1.5v-3a1.5 1.5 0 00-1.5-1.5h-2.136l-6 6zM5 16a1 1 0 100-2 1 1 0 000 2z\" fill-rule=\"evenodd\" />\n</svg>",
-        // "url": "/getting-started/mjml-integration"
-    }
-]
